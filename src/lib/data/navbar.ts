c2e09b4b77413016ca6215c169841c1f0ddfc543
Anish Philip <anishphilip012git@live.in>
2025-02-02 23:13:07 -0500

    commenting musings for a while

--- src/lib/data/navbar.ts
+++ src/lib/data/navbar.ts
@@ -1,9 +1,9 @@
 
 export const items = [
-	{ title: 'Skills', to: '/skills', icon: 'i-carbon-software-resource-cluster' ,iconType:'uicon'},
-	{ title: 'Projects', to: '/projects', icon: 'i-carbon-cube' ,iconType:'uicon'},
-	{ title: 'Experience', to: '/experience', icon: 'i-carbon-development'  ,iconType:'uicon'},
-	{ title: 'Education', to: '/education', icon: 'i-carbon-education'  ,iconType:'uicon'},
-	{ title: 'Resumé', to: '/resume', icon: 'i-carbon-result'  ,iconType:'uicon'},
-	{title:'Musings', to:'/musings',icon:'twemoji:writing-hand-medium-skin-tone',iconType:'twemoji'}
+	{ title: 'Skills', to: '/skills', icon: 'i-carbon-software-resource-cluster', iconType: 'uicon' },
+	{ title: 'Projects', to: '/projects', icon: 'i-carbon-cube', iconType: 'uicon' },
+	{ title: 'Experience', to: '/experience', icon: 'i-carbon-development', iconType: 'uicon' },
+	{ title: 'Education', to: '/education', icon: 'i-carbon-education', iconType: 'uicon' },
+	{ title: 'Resumé', to: '/resume', icon: 'i-carbon-result', iconType: 'uicon' },
+	// {title:'Musings', to:'/musings',icon:'twemoji:writing-hand-medium-skin-tone',iconType:'twemoji'}
 ] as const;
